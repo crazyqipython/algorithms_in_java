0ff0dc6f993c6938ece30926685f33a7a63abe51
Taylor <huangqihyper@gmail.com>
2022-10-17 16:04:25 +0800

    1017

--- Array/RemoveDuplicatesFromSortedArray.java
+++ Array/RemoveDuplicatesFromSortedArray.java
@@ -2,5 +2,20 @@ package Array;
 import java.util.*;
 
 public class RemoveDuplicatesFromSortedArray {
-    public int RemoveDuplicatesFromSortedArray(int[] nums){
-        
+    public int removeDuplicates(int[] nums) {
+        int i = 0;
+
+        for(int j = 1; j<nums.length; j++){
+            if(nums[i] == nums[j]){
+                continue;
+            }else{
+                i++;
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+        }
+
+        return i+1;
+    }
+}
